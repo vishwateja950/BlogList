c408a7a92efe8b2ed6f6fba2c43aae3aaf033f9a
vishwateja950 <vishwatejapatel1234@gmail.com>
2022-08-20 11:09:24 +0000

    solution

---
+++ src/components/BlogsList/index.js
@@ -0,0 +1,45 @@
+import {Component} from 'react'
+
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+class BlogsList extends Component {
+  state = {isLoading: true, blogsData: []}
+
+  componentDidMount() {
+    this.getBlogsData()
+  }
+
+  getBlogsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const statusCode = await response.statusCode
+    console.log(statusCode)
+    const data = await response.json()
+
+    const formattedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+      author: eachItem.author,
+      topic: eachItem.topic,
+    }))
+    this.setState({blogsData: formattedData, isLoading: false})
+  }
+
+  render() {
+    const {blogsData, isLoading} = this.state
+    console.log(isLoading)
+
+    return (
+      <div className="blog-list-container">
+        {blogsData.map(item => (
+          <BlogItem blogData={item} key={item.id} />
+        ))}
+      </div>
+    )
+  }
+}
+
+export default BlogsList
